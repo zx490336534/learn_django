df7eb22d6d1e0a2ecf33d115cb8948b2042882b1
zhongxin <490336534@qq.com>
2018-11-06 22:32:25 +0800

    5、创建映射 python manage.py makemigrations book 6、创建表 python manage.py migrate book

--- hello_dj/book/models.py
+++ hello_dj/book/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)  # 自增长,可以省略
+    name = models.CharField(max_length=30)  # char类型最大长度30
+    age = models.IntegerField()  # Int类型
