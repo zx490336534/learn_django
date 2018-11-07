5fef4b7185f98db19471193152fd35a668b49e59
zhongxin <490336534@qq.com>
2018-11-07 18:52:30 +0800

    新增一个book_test1表

--- hello_dj/book/models.py
+++ hello_dj/book/models.py
@@ -7,3 +7,13 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)  # 自增长,可以省略
     name = models.CharField(max_length=30)  # char类型最大长度30
     age = models.IntegerField()  # Int类型
+
+
+class Test1(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+# 配置连接
+# 创建模型类
+# 创建映射文件 makemigrations
+# 生成数据表 migrate
